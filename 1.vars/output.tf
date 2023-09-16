695383b23eb9bed6d8b891fc889641c667f82946
neeliveti <neeliveti.kirankumar@gmail.com>
2023-09-16 20:26:41 +0530

    terraform

---
+++ 1.vars/output.tf
@@ -0,0 +1,27 @@
+output "sample_string" {
+    value=var.sample_string   ### this is nothing to $sample_string {var.variable name}
+}
+### in above case we are printing just varible. if variable is a combination of some string 
+###then this is the syntax. ${ } is mandatory if we include the variable inside a string
+
+output "sample_string1" {
+    value= "value of sample_string = ${var.sample_string}"
+}
+
+
+
+### values from list
+variable  "sample_list"  {
+    default =[
+        100,
+        hellow,
+        false,
+        101
+    ]
+}
+
+## values from list
+## second value
+output "sample_list_2" {
+    value = var.sample_list[2]
+}
