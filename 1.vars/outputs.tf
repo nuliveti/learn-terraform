f82a4082f75f6ebdc480ae858e86cefc9912e495
neeliveti <neeliveti.kirankumar@gmail.com>
2023-09-17 11:25:33 +0530

    terraform

---
+++ 1.vars/outputs.tf
@@ -0,0 +1,17 @@
+##to print in shell script will use ECHO, in terraform as follows
+## echo $sample_string
+### in terraform like below we will refer variable
+
+
+output "sample_string" {
+    value = var.sample_string
+}
+
+
+### in above case we are printing just variable, if variable is a combination of some string then  this is the syntax
+### ${ }is mandatory if we include  the variable inside a string 
+
+
+output "sample_string1" {
+    value = "value of sample_string = ${var.sample_string}"
+}
