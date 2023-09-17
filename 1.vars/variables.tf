29d95e16e93f032bb03ec26c90d1b01cbac33afb
neeliveti <neeliveti.kirankumar@gmail.com>
2023-09-17 11:34:32 +0530

    terraform

--- 1.vars/variables.tf
+++ 1.vars/variables.tf
@@ -9,4 +9,20 @@ variable "sample_string" {
 ##access our variable:
 
 
+variable "sample_number" {
+    default = 100
+}
+
+variable "sample_boolean" {
+    default = true
+}
+
+
+output "sample_string1" {
+    value = "value of sample_boolean = ${var.sample_boolean}"
+}
+## booleans and numbers does not need to quote, only strings need to be quoted that to be with double quote, terraform does not have support for single quotes.
+
+### variable types:default and list variables
+
 
