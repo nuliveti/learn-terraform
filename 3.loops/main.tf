fcd179f9122fc912518b706cf74109c6a4ea4bc4
neeliveti <neeliveti.kirankumar@gmail.com>
2023-09-20 11:22:16 +0530

    loops

--- 3.loops/main.tf
+++ 3.loops/main.tf
@@ -1,3 +1,11 @@
 resource "null_resource" "null" {
     count = 10
 }
+
+resource "null_resource" "fruits" {
+    count = length(var.fruits)
+}
+
+variable "fruits" {
+    dafault = {"apple", "banana", ""orange"}
+} 
